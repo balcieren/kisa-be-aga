1b804c442975717588ea1d556db8469e3e69544d
Eren BALCI <erenbalci3939@gmail.com>
2021-06-28 12:47:38 +0300

    added styled-component-ssr

---
+++ web/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }: any) {
+    return renderPage();
+  }
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>My page</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
