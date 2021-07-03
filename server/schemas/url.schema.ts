dd3309334c62bc2c8ecc03c31b6b42773f7b97ca
Eren BALCI <erenbalci3939@gmail.com>
2021-07-03 23:25:54 +0300

    Update url.schema.ts

--- server/schemas/url.schema.ts
+++ server/schemas/url.schema.ts
@@ -1,14 +1,13 @@
-import { FastifySchema } from "fastify";
-import * as yup from "yup";
+import { createYupSchema } from "fastify-yup-schema";
 
-export const redirectToShortUrlSchema: FastifySchema = {
+export const redirectToShortUrlSchema = createYupSchema((yup) => ({
   params: yup.object({ shortid: yup.string().required() }).required(),
-};
+}));
 
-export const getShortUrlStatusSchema: FastifySchema = {
+export const getShortUrlStatusSchema = createYupSchema((yup) => ({
   params: yup.object({ shortid: yup.string().required() }).required(),
-};
+}));
 
-export const createShortUrlSchema: FastifySchema = {
+export const createShortUrlSchema = createYupSchema((yup) => ({
   body: yup.object({ url: yup.string().url().required() }).required(),
-};
+}));
