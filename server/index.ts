c79a26011b4eb4e5ad114e2cc40aee32e335e56f
Eren BALCI <erenbalci3939@gmail.com>
2021-07-03 23:25:43 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,17 +2,18 @@ import Fastify, { FastifyInstance } from "fastify";
 import fastifySensible from "fastify-sensible";
 import fastifyPrisma from "fastify-prisma-client";
 import urlRoute from "./routes/url.route";
-import schemaValidator from "./middlewares/validator.middleware";
+import { fastifyYupSchema } from "fastify-yup-schema";
 
 const buildFastify = () => {
   const fastify: FastifyInstance = Fastify({
     logger: { level: "info", prettyPrint: true },
   });
+
   fastify
     .register(fastifyPrisma)
     .register(fastifySensible)
-    .register(urlRoute, { prefix: "/api/url" })
-    .setValidatorCompiler(schemaValidator);
+    .register(fastifyYupSchema)
+    .register(urlRoute, { prefix: "/api/url" });
 
   fastify.get("/", async () => {
     return "KISA BE AGA !";
