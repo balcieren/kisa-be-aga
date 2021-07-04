bd788bf06b0a4f587ef0cafd0002abc58c79e434
Eren BALCI <erenbalci3939@gmail.com>
2021-07-04 17:19:00 +0300

    updated colors

--- web/theme/colors.ts
+++ web/theme/colors.ts
@@ -1,1 +1,9 @@
-const colors = {};
+export const colors = {
+  background: "#D0DFFF",
+  success: "#17C964",
+  warning: "#FFBA00",
+  primary: "#1A5CFF",
+  danger: "#FF4751",
+};
+
+export type ThemeColors = typeof colors;
