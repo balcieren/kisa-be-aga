244b4fa1be0af4924aa1bdd1cbeef85c309019d2
Eren BALCI <erenbalci3939@gmail.com>
2021-09-07 17:12:47 +0300

    removed files

--- server/schemas/url.schema.ts
+++
@@ -1,8 +0,0 @@
-import { FastifySchema } from "fastify";
-import { urlBody, urlParams } from "../dtos/url.dto";
-
-export const redirectToShortUrlSchema: FastifySchema = { params: urlParams };
-
-export const getShortUrlStatusSchema: FastifySchema = { params: urlParams };
-
-export const createShortUrlSchema: FastifySchema = { body: urlBody };
