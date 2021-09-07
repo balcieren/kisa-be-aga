944e3ce53decc30988233ae540513638f5d47ac2
Eren BALCI <erenbalci3939@gmail.com>
2021-09-07 17:17:43 +0300

    added swagger tag

--- server/src/modules/url/schemas.ts
+++ server/src/modules/url/schemas.ts
@@ -1,8 +1,8 @@
 import { FastifySchema } from "fastify";
 import { urlBodyValidate, urlParamsValidate } from "./validates";
 
-export const redirectToShortUrlSchema: FastifySchema = { params: urlParamsValidate };
+export const redirectToShortUrlSchema: FastifySchema = { tags: ["url"], params: urlParamsValidate };
 
-export const getShortUrlStatusSchema: FastifySchema = { params: urlParamsValidate };
+export const getShortUrlStatusSchema: FastifySchema = { tags: ["url"], params: urlParamsValidate };
 
-export const createShortUrlSchema: FastifySchema = { body: urlBodyValidate };
+export const createShortUrlSchema: FastifySchema = { tags: ["url"], body: urlBodyValidate };
